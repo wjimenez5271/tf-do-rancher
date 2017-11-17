46e3f5ac7cacff63608da24f043d96f9fdea55fb
William Jimenez <william@rancher.com>
2017-11-17 10:38:17 +0900

    parameterize node count

--- nodes/main.tf
+++ nodes/main.tf
@@ -8,6 +8,10 @@ variable "ssh_keys" {
   type = "list"
 }
 
+variable "node_count" {
+  default = 2
+}
+
 data "terraform_remote_state" "rancher-stack" {
   backend = "local"
 
@@ -19,7 +23,7 @@ data "terraform_remote_state" "rancher-stack" {
 
 module "digital-ocean" {
   source = "../modules/digital-ocean"
-  node_count = 2
+  node_count = "${var.node_count}"
   digitalocean_token = "${var.digitalocean_token}"
   docker_cmd = "${data.terraform_remote_state.rancher-stack.registration-token}"
   ssh_keys = "${var.ssh_keys}"
